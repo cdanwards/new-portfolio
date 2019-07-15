4db9afce0624ed8fd3e4d915af04f0ae605d0c53
Daniel Edwards <dan.edwards@newspring.cc>
2019-07-15 13:20:55 -0400

    Initial Pass

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,11 +8,69 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div style={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }}>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          width: `100%`,
+        }}
+      >
+        <div
+          style={{
+            width: `50%`,
+            height: "500px",
+            background: "salmon",
+          }}
+        >
+          <h1>Hi people</h1>
+          <p>Welcome to your new Gatsby site.</p>
+          <p>Now go build something great.</p>
+        </div>
+        <div
+          style={{
+            width: `50%`,
+            height: "500px",
+            background: "cyan",
+          }}
+        >
+          <h1>Hi people</h1>
+          <p>Welcome to your new Gatsby site.</p>
+          <p>Now go build something great.</p>
+        </div>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          width: `100%`,
+        }}
+      >
+        <div
+          style={{
+            width: `50%`,
+            height: "500px",
+            background: "goldenrod",
+          }}
+        >
+          <h1>Hi people</h1>
+          <p>Welcome to your new Gatsby site.</p>
+          <p>Now go build something great.</p>
+        </div>
+        <div
+          style={{
+            width: `50%`,
+            height: "500px",
+            background: "blue",
+          }}
+        >
+          <h1>Hi people</h1>
+          <p>Welcome to your new Gatsby site.</p>
+          <p>Now go build something great.</p>
+        </div>
+      </div>
     </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
