b80356c23cf6263e475c36e3b7066cbb7ddea023
cdanwards <cdaniel.edwards@gmail.com>
2021-12-04 13:38:48 -0500

    Updated Copy

--- src/components/boxes.js
+++ src/components/boxes.js
@@ -110,13 +110,13 @@ const Boxes = () => (
         <WhiteText>Thanks for stopping by!</WhiteText>
         <WhiteText>
           My name is Dan Edwards and I'm a software developer based out of
-          Greenville, SC currently working as a Software Engineer @{" "}
-          <StyledA2 href="https://orangebees.com">Orange Bees</StyledA2>.
+          Greenville, SC currently working as a Javascript Engineer @{" "}
+          <StyledA2 href="https://mywell.org">My Well</StyledA2>.
         </WhiteText>
         <WhiteText>
           I'm passionate about providing great user experiences, creating fun
           and clean designs, and solving difficult problems. I'm currently
-          learning more about Flutter, Vue and TypeScript.
+          learning more about Remix, Web3 and NextJS.
         </WhiteText>
       </OrangeBox>
       <div style={{ margin: "0 auto"}}>
@@ -135,13 +135,14 @@ const Boxes = () => (
         <Whiteh1> about me </Whiteh1>
         <WhiteText>
           When I'm not developing, you can find me going on adventures with my
-          wonderful wife and our dog, improving our fixer-upper of a house, and
-          playing slightly above average Magic The Gathering.
+          wonderful wife and one year old daughter, tackling projects for our 
+          fixer-upper of a house, and playing slightly 
+          above average Magic The Gathering.
         </WhiteText>
         <WhiteText>
-          Since graduating from the Iron Yard Academy, I've strived to
-          continuously learn and improve my skills to create modern web and
-          mobile apps.
+          Since graduating from the Iron Yard Academy and over the course of my career, 
+          I've strived to continuously learn and master my skillset to create 
+          modern web and mobile apps.
         </WhiteText>
       </BlueBox>
     </RowReverseContainer>
